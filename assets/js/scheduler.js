743222f32961d4a7c0e07d524c94a66533bfbcc9
Jon Harrison <jonathan_harrison@hotmail.com>
2022-12-04 09:41:56 +0000

    implement clock using jQuery and moment.js methods

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -1,1 +1,9 @@
-"use strict;"
+
+(function($) {
+    // clock
+    $(function() {
+        timer = setInterval(function () {
+            $('#currentDay').text(moment().format("dddd Do MMMM YYYY HH:mm:ss"));
+        }, 1000);
+    });
+})(jQuery);
